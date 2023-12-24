145fd9243d1ac6add0785bfc5bef646763384748
Ankit Sharma <sharmaa7861@gmail.com>
2023-12-24 21:16:58 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center text-center w-full">
+      <h1 className="font-light text-3xl">News-App</h1>
+    </div>
+  );
+}
+
+export default App;
