621db839aa9ae3bc43c02bdd2ffd7efa40ddc136
Ankit Sharma <sharmaa7861@gmail.com>
2023-12-25 21:23:48 +0530

    updating changes: fetching news data

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+// import React, { useEffect, useState } from "react";
+// import fetchNews from "../apis/fetchNews.tsx";
+// import { categories } from "../constants";
 
-const Homepage =  () => {
+const Homepage = () => {
+  // const [news, setNews] = useState([]);
+
+  // useEffect(() => {
+  //   const apiUrl =
+  //     "http://api.mediastack.com/v1/news?access_key=a7955be8ccc3a113acb18bc69f38589f";
+
+  //   fetch(apiUrl)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setNews(data);
+  //       console.log(data);
+  //     })
+  //     .catch((error) => console.error("Error:", error));
+  // }, []);
+
+  // const news = async () => {
+  //   await fetchNews(categories.join(","));
+  // };
+  // console.log(news);
+
   return (
     <div>
-      <p>Homepage</p>
+      <p>Home Page</p>
     </div>
   );
 };
 
 export default Homepage;
-// http://api.mediastack.com/v1/news?access_key=a7955be8ccc3a113acb18bc69f38589f
